c9d712380e664326d8ff5cb5428f9927b6896974
Miha Rekar <miha.rekar@gmail.com>
2013-03-23 12:14:03 +0100

    .blank? FTW and some cs optimisations

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,11 @@
 class ApiController < ApplicationController
   def restaurants
-    if params[:search]
-      restaurants = Restaurant.search(params[:search])
-    else
+    if params[:search].blank?
       restaurants = Restaurant.all
+    else
+      restaurants = Restaurant.where('name ILIKE :search OR address ILIKE :search', search: '%' + params[:search] + '%')
     end
-    
+
     render :json => restaurants.each{|r| r[:link] += '0'}.to_json(only:[:name, :address, :price, :coordinates, :opening, :link, :menu], include:{:features => {:only => [:feature_id, :title]}})
   end
 end
--- app/controllers/prehrana_controller.rb
+++ app/controllers/prehrana_controller.rb
@@ -1,24 +1,24 @@
-class PrehranaController < ApplicationController  
+class PrehranaController < ApplicationController
   def index
     @features = Feature.all
   end
-  
+
   def all_restaurants
     render :json => Restaurant.all.to_json(only:[:id, :coordinates, :price], methods:[:content, :feature_ids])
   end
-  
-  def search    
-    if params[:features] and not params[:features].empty?
+
+  def search
+    if !params[:features].blank?
       restaurant_ids = Restaurant.find_by_sql(['SELECT restaurant_id FROM (
-                                                  SELECT features_restaurants.*, ROW_NUMBER() OVER(PARTITION BY restaurants.id ORDER BY features.id) AS rn FROM restaurants 
+                                                  SELECT features_restaurants.*, ROW_NUMBER() OVER(PARTITION BY restaurants.id ORDER BY features.id) AS rn FROM restaurants
                                                   JOIN features_restaurants ON restaurants.id = features_restaurants.restaurant_id
                                                   JOIN features ON features_restaurants.feature_id = features.id
                                                   WHERE features.id in (?)
                                                 ) t
                                                 WHERE rn = ?', params[:features], params[:features].count]).map(&:restaurant_id)
     end
-    
-    if params[:search] and not params[:search].empty?
+
+    if !params[:search].blank?
       if restaurant_ids
         restaurants = Restaurant.where('(name ILIKE :search OR address ILIKE :search) AND id IN (:ids)', search: '%' + params[:search] + '%', ids: restaurant_ids)
       else
@@ -26,12 +26,12 @@ class PrehranaController < ApplicationController
       end
       restaurant_ids = restaurants.map(&:id)
     end
-    
+
     render :json => restaurant_ids
   end
-  
+
   def menu
-    if params[:restaurant]
+    if !params[:restaurant].blank?
       menu = Restaurant.select(:menu).find(params[:restaurant])[:menu]
       content = '<ol class="menu">'
       menu.each do |menu_item|
