f6d1ef2a301938a16371b1d2bcb2623a2a41832e
Miha Rekar <miha.rekar@gmail.com>
2013-04-29 23:25:29 -0700

    rails 4 rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Bonar::Application.config.session_store :encrypted_cookie_store, key: '_bonar_session'
+Bonar::Application.config.session_store :cookie_store, key: '_bonar_session'
