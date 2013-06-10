19bda75022a71476a29c02a262afa24bee1a50c8
Miha Rekar <miha.rekar@gmail.com>
2013-06-10 10:23:42 +0200

    order features by title

--- app/controllers/prehrana_controller.rb
+++ app/controllers/prehrana_controller.rb
@@ -1,18 +1,18 @@
 class PrehranaController < ApplicationController
   def index
-    @features = Feature.all
+    @features = Feature.all.order(:title)
   end
 
   def all_restaurants
     @restaurants = Restaurant.where(disabled: false)
     fresh_when(@restaurants.first)
   end
-  
+
   def menu
     @restaurant = Restaurant.find(params[:restaurant])
     render layout: false
   end
-  
+
   def content
     @restaurant = Restaurant.find(params[:restaurant])
     render layout: false
