a7b19307d8e03e179960c227a864693f096f1730
Miha Rekar <info@mr.si>
2013-09-29 18:02:39 +0200

    precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,9 @@ Bonar::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.precompile += %w( prehrana.js list* )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
