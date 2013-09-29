25f083df77165d049d6dee77c2fdf8893d9e15c1
Miha Rekar <info@mr.si>
2013-09-29 18:13:56 +0200

    there is no navigation so we dont need navbar toggle

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,8 @@ module Bonar
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  
+
     # Precompile additional assets.
     # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-    config.assets.precompile += %w( prehrana.js )
   end
 end
