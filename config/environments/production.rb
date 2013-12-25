a28535fc0683360b47ef6959dcb121bfb464a30c
Miha Rekar <info@mr.si>
2013-12-25 13:24:45 +0100

    3 unicorn workers, :warn logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,14 +43,15 @@ Bonar::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
+  config.logger = Logger.new(STDOUT)
+  
   # Use a different cache store in production.
   # config.cache_store = :dalli_store
 
