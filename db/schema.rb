7fc8a2b76816b46cc218472d4201ac22d5ac46fc
Miha Rekar <info@mr.si>
2014-01-25 19:38:32 +0100

    remove link from Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125183109) do
+ActiveRecord::Schema.define(version: 20140125183754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20140125183109) do
     t.string   "price"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "link"
     t.string   "spid"
     t.text     "opening"
     t.text     "menu"
