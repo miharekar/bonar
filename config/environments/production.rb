485d8b0d5b284d65914a2af875ff52e702ed7544
Miha Rekar <info@mr.si>
2014-06-30 19:40:10 +0200

    deflate assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Bonar::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
+  # Deflate them assets
+  config.middleware.insert_before('ActionDispatch::Static', Rack::Deflater)
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
