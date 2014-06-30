787bf0caa9dfe0d80a24029eda7ba7db25dcf76b
Miha Rekar <info@mr.si>
2014-06-30 22:28:43 +0200

    use transactional fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -24,5 +23,6 @@ end
 RSpec.configure do |config|
   config.before(:suite) { FactoryGirl.reload }
   config.include FactoryGirl::Syntax::Methods
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
